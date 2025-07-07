e4686195c5869a9af94dfe737590f2bc2f892066
Alfredo Fung <116277334+C102002@users.noreply.github.com>
2025-07-06 20:57:30 -0400

    add-test

---
+++ test/reservation/test_find_active_reservation.py
@@ -0,0 +1,21 @@
+from fastapi.testclient import TestClient
+from src.main import app
+import pytest
+from dotenv import load_dotenv
+import asyncio
+import platform
+
+if platform.system() == "Windows":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+load_dotenv()
+
+client = TestClient(app)
+
+@pytest.mark.asyncio
+async def test_reservation_find_active_sucess():
+    response = client.post("/reservation/find-active/197dd255-a202-4aed-b973-bc7af39ee411", json=data)
+    assert response.status_code == 201
+    #response_data = response.json()
+    #assert response_data is None
+    
